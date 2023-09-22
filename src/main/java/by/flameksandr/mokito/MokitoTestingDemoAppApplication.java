f5edc5f8d6df1a03a63d7c7fec7c8573c8be8a12
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-22 21:48:50 +0300

    Initial commit

---
+++ src/main/java/by/flameksandr/mokito/MokitoTestingDemoAppApplication.java
@@ -0,0 +1,13 @@
+package by.flameksandr.mokito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokitoTestingDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MokitoTestingDemoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/flameksandr/mokito/MokitoTestingDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.flameksandr.mokito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MokitoTestingDemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
